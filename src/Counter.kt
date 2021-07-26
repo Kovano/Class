7385aaa4bc26c6df5b89c4a1fe91f78356220c35
Kovano <kovano@itkn.ru>
2021-07-26 21:00:32 +0300

    first commit

---
+++ src/Counter.kt
@@ -0,0 +1,5 @@
+class Counter {
+    var count = 0 // а вот это уже свойство
+    fun inc(){ count++} // а это метод
+    fun dec(){ count--} // и это метод
+}
---
+++ src/pp01.kt
@@ -0,0 +1,11 @@
+// Вот тут мы смотрим, что такое классы, свойства и методы.
+// и видео с пояснениями https://youtu.be/jBRoxSfb44g
+fun main() {
+
+    var counter = Counter()
+    counter.dec(); counter.inc(); counter.inc()
+    println(counter.count)
+    var counter5 = Counter ()
+    counter5.inc(); counter5.inc()
+    println(counter5.count)
+    }
